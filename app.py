760b48de02e600ec79ffbbf987955c04407734fd
Bagus Viki Amalindo <vikiamalindo7@gmail.com>
2025-11-12 13:40:32 +0700

    update test

--- app.py
+++ app.py
@@ -149,5 +149,5 @@ def cetak_pdf():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
